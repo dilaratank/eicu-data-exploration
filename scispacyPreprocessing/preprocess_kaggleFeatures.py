e613fa63ce034d325320270171c2904a4fa89569
DTank <dtank@amsterdamumc.nl>
2023-02-03 09:22:40 +0000

    updated data exploration

--- scispacyPreprocessing/preprocess_kaggleFeatures.py
+++ scispacyPreprocessing/preprocess_kaggleFeatures.py
@@ -4,4 +4,4 @@ notes = pd.read_csv("/home/dtank/TempData/scispacyRaw/features.csv") #dataset fr
 notes['feature_text'] = notes['feature_text'].replace("-", " ", regex=True)
 notes["feature_text"] = notes["feature_text"].replace("OR", ",", regex=True)
 
-notes.to_csv("/home/dtank/TempData/scispacyPreprocessed/preprocessedFeatures.csv")
+notes.to_csv("/home/dtank/data/volume_2/scispacyPreprocessed/preprocessedFeatures.csv")
